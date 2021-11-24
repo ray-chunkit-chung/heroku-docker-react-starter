5c6748d73725af18d842add0bd97c825e4b48e52
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2021-11-24 17:25:36 +0900

    for shining fans

--- src/App.js
+++ src/App.js
@@ -11,7 +11,43 @@ class App extends Component {
           <h1 className="App-title">Hello world! x4</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
+        </p>
+        <p className="App-intro">
+          All work and no play makes jack a dull boy.
         </p>
       </div>
     );
