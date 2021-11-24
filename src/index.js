78dece8bc038ea3c83b1059997e152da2efcef3e
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2021-11-24 17:17:41 +0900

    change the app to hello world

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-express()
-  .use(express.static(path.join(__dirname, '..', 'public')))
-  .set('views', path.join(__dirname, '..', 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
